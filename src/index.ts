bcddd0156c8e432722da01ec201f225020c167ca
davidgreenspan15 <ramigreenspangmail.com>
2021-07-16 15:03:19 -0400

    testing cors 2

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,24 @@ import cron from 'node-cron';
 const app = express();
 const PORT = process.env.PORT || 8000;
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Credentials', 'false');
-  res.setHeader('Access-Control-Allow-Methods', '*'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+  // Request methods you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
   next();
 });
 app.use(express.json());
