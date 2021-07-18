81a47bbc1c6bae7a8dd1a015ec3fd8bdef51573e
davidgreenspan15 <ramigreenspangmail.com>
2021-07-18 07:52:54 -0400

    testing cor6

--- src/index.ts
+++ src/index.ts
@@ -8,23 +8,15 @@ import cron from 'node-cron';
 const app = express();
 const PORT = process.env.PORT || 8000;
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  );
+  res.setHeader('Access-Control-Allow-Methods', '*');
 
   // Request headers you wish to allow
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-Requested-With,Content-Type'
-  );
+  res.setHeader('Access-Control-Allow-Headers', '*');
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  // res.setHeader('Access-Control-Allow-Credentials', 'false');
 
   next();
 });
